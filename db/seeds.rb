3a26a721611984fdc5a61f42d28b2d7e536fba12
shiroemons <shiroemons@gmail.com>
2018-05-04 23:14:07 +0900

    Artistの設定時に例外を発生するように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-circle = Circle.find_or_create_by(name_ja: '上海アリス幻樂団', name_en: 'Team Shanghai Alice', circle_type: 'originator')
-circle.update_attributes(site_url: 'http://www16.big.or.jp/~zun/', blog_url: 'http://kourindou.exblog.jp/')
-artist = Artist.find_or_create_by(name_ja: 'ZUN', name_en: 'ZUN', circle: circle)
-artist.update_attributes(site_url: 'http://www16.big.or.jp/~zun/', blog_url: 'http://kourindou.exblog.jp/')
+circle = Circle.find_or_create_by!(name_ja: '上海アリス幻樂団', name_en: 'Team Shanghai Alice', circle_type: 'originator')
+circle.update!(site_url: 'http://www16.big.or.jp/~zun/', blog_url: 'http://kourindou.exblog.jp/')
+artist = Artist.find_or_create_by!(name_ja: 'ZUN', name_en: 'ZUN', circle: circle)
+artist.update!(site_url: 'http://www16.big.or.jp/~zun/', blog_url: 'http://kourindou.exblog.jp/')
 artist.arranger || artist.create_arranger
 artist.composer || artist.create_composer
 
-circle = Circle.find_or_create_by(name_ja: '黄昏フロンティア', name_en: 'Twilight Frontier', circle_type: 'originator')
-circle.update_attributes(site_url: 'https://www.tasofro.net/', blog_url: 'http://tasofro.net/diary/index.php')
-artist = Artist.find_or_create_by(name_ja: 'あきやまうに', name_en: 'U2', circle: circle)
-artist.update_attributes(site_url: 'https://www.tasofro.net/', blog_url: 'http://tasofro.net/diary/index.php')
+circle = Circle.find_or_create_by!(name_ja: '黄昏フロンティア', name_en: 'Twilight Frontier', circle_type: 'originator')
+circle.update!(site_url: 'https://www.tasofro.net/', blog_url: 'http://tasofro.net/diary/index.php')
+artist = Artist.find_or_create_by!(name_ja: 'あきやまうに', name_en: 'U2', circle: circle)
+artist.update!(site_url: 'https://www.tasofro.net/', blog_url: 'http://tasofro.net/diary/index.php')
 artist.arranger || artist.create_arranger
 artist.composer || artist.create_composer
