a806dc22e0e0c4d43c43f86e6e2c3e8499d734b6
shiroemons <shiroemons@gmail.com>
2018-05-05 14:06:54 +0900

    Artistのspecを追加

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'has_one' do
+    it { is_expected.to have_one(:vocalist) }
+    it { is_expected.to have_one(:arranger) }
+    it { is_expected.to have_one(:lyricist) }
+    it { is_expected.to have_one(:composer) }
+    it { is_expected.to have_one(:rearranger) }
+  end
+
+  describe 'belongs_to' do
+    it { is_expected.to belong_to(:circle) }
+    it { is_expected.to belong_to(:user) }
+  end
 end
