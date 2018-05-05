2597a211ea8e61c45f1b5d3b2c81e155faa22352
shiroemons <shiroemons@gmail.com>
2018-05-05 14:24:12 +0900

    Eventのspecを追加

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'has_many' do
+    it { is_expected.to have_many(:discographies) }
+    it { is_expected.to have_many(:songs) }
+  end
+
+  describe 'belongs_to' do
+    it { is_expected.to belong_to(:event_series) }
+  end
+
+  describe 'delegations' do
+    it { is_expected.to delegate_method(:title_ja).to(:event_series).with_prefix(true) }
+    it { is_expected.to delegate_method(:title_en).to(:event_series).with_prefix(true) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:title_ja) }
+    it { is_expected.to validate_presence_of(:display_title_ja) }
+  end
 end
