867feb255c89decafa17004e2e65630298b0ffc0
shiroemons <shiroemons@gmail.com>
2018-05-05 14:15:09 +0900

    Discographyのspecを追加

--- spec/models/discography_spec.rb
+++ spec/models/discography_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Discography, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'has_many' do
+    it { is_expected.to have_many(:songs) }
+  end
+
+  describe 'belongs_to' do
+    it { is_expected.to belong_to(:circle) }
+    it { is_expected.to belong_to(:event) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:title_ja) }
+  end
 end
