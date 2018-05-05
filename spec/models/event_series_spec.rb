29756b3b38b4fba2f3fa177300acfe2d54409089
shiroemons <shiroemons@gmail.com>
2018-05-05 14:17:04 +0900

    EventSeriesのspecを追加

--- spec/models/event_series_spec.rb
+++ spec/models/event_series_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe EventSeries, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'has_many' do
+    it { is_expected.to have_many(:event) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:title_ja) }
+  end
 end
