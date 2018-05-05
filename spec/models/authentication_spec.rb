fb59c39b6e03942fb337d74a517ee823c7fc0512
shiroemons <shiroemons@gmail.com>
2018-05-05 15:23:41 +0900

    Authenticationのspecを追加

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Authentication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'belongs_to' do
+    it { is_expected.to belong_to(:user) }
+  end
 end
