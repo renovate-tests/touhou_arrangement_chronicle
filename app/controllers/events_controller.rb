74090e207c72a6bd99cd4e1aea796c53f6744ab6
shiroemons <shiroemons@gmail.com>
2018-05-19 10:08:16 +0900

    [WIP]イベント別のシリーズ別・日付別の項目別のリストを追加(リンクなし)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
   def index
+    @event_series = EventSeries.order(:display_order)
+    @event_years = Event.distinct.pluck(Arel.sql("date_part('year', date)")).compact.sort.reverse.map(&:to_i)
   end
 end
